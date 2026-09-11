337c74a55bf9ab0bacafad34757137561d0df46b
Amoifr <pascal.cescon@gmail.com>
2026-09-11 09:58:46 +0200

    [DependencyInjection] Add the "kernel.close" tag to close services on kernel shutdown

--- src/Symfony/Component/DependencyInjection/CHANGELOG.md
+++ src/Symfony/Component/DependencyInjection/CHANGELOG.md
@@ -4,6 +4,7 @@ CHANGELOG
 8.2
 ---
 
+ * Add the `kernel.close` tag and the `services_closer` service to close services that hold resources when the kernel shuts down
  * Name the package to install when an extension is missing, for the configuration keys declared in the `.container.extension_packages` build parameter
  * Add the `container.remove_if_missing` tag to drop a definition when a service, a class or a package it needs is not there
  * Add `ContainerBuilder::setExtensionConfig()`
--- src/Symfony/Component/DependencyInjection/Tests/AbstractKernelTest.php
+++ src/Symfony/Component/DependencyInjection/Tests/AbstractKernelTest.php
@@ -20,7 +20,9 @@ use Symfony\Component\DependencyInjection\Kernel\AbstractKernel;
 use Symfony\Component\DependencyInjection\Kernel\BundleInterface;
 use Symfony\Component\DependencyInjection\Kernel\KernelTrait;
 use Symfony\Component\DependencyInjection\Kernel\RequiredBundle;
+use Symfony\Component\DependencyInjection\Kernel\ServicesBundle;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\DependencyInjection\Tests\Fixtures\ClosableService;
 use Symfony\Component\Filesystem\Filesystem;
 
 class AbstractKernelTest extends TestCase
@@ -195,6 +197,55 @@ class AbstractKernelTest extends TestCase
         $kernel = $this->createKernelWithBundles([$bundle]);
         $kernel->boot();
         $kernel->shutdown();
+    }
+
+    public function testShutdownClosesTaggedServices()
+    {
+        ClosableService::$closed = 0;
+        $this->writeBundlesFile([ServicesBundle::class]);
+        $kernel = new ClosingKernel('test', true, $this->varDir);
+        $kernel->boot();
+        $kernel->getContainer()->get('closable');
+
+        $kernel->shutdown();
+
+        $this->assertSame(1, ClosableService::$closed);
+    }
+
+    public function testShutdownDoesNotInstantiateServicesToCloseThem()
+    {
+        ClosableService::$closed = 0;
+        $this->writeBundlesFile([ServicesBundle::class]);
+        $kernel = new ClosingKernel('test', true, $this->varDir);
+        $kernel->boot();
+
+        $kernel->shutdown();
+
+        $this->assertSame(0, ClosableService::$closed);
+    }
+
+    public function testShutdownClearsContainerWhenClosingFails()
+    {
+        ClosableService::$closed = 0;
+        $this->writeBundlesFile([ServicesBundle::class]);
+        $kernel = new ClosingKernel('test', true, $this->varDir);
+        $kernel->boot();
+        $kernel->getContainer()->get('failing');
+        $kernel->getContainer()->get('closable');
+
+        try {
+            $kernel->shutdown();
+            $this->fail('The exception thrown while closing a service should not be swallowed.');
+        } catch (\RuntimeException $e) {
+            $this->assertSame('Cannot close.', $e->getMessage());
+        }
+
+        // one service failing to close does not prevent closing the others
+        $this->assertSame(1, ClosableService::$closed);
+        $this->assertFalse($kernel->isBooted());
+
+        $this->expectException(\LogicException::class);
+        $kernel->getContainer();
     }
 
     public function testGetBundlesAndGetBundle()
@@ -625,6 +676,19 @@ class BuildHookKernel extends TestKernel
     protected function build(ContainerBuilder $container): void
     {
         $container->setParameter('build_hook_called', true);
+    }
+}
+
+class ClosingKernel extends TestKernel
+{
+    protected function build(ContainerBuilder $container): void
+    {
+        $container->register('failing', ClosableService::class)
+            ->setPublic(true)
+            ->addTag('kernel.close', ['method' => 'fail']);
+        $container->register('closable', ClosableService::class)
+            ->setPublic(true)
+            ->addTag('kernel.close');
     }
 }
 
